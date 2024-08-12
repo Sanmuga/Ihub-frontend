5466b5aa6328efc6a56e1b268cbeecfbfdb479cb
sanmuga <sanmugavadivel23@gmail.com>
2024-08-12 11:55:01 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>THIS IS SANMUGA</p>
+        <p>TESTING</p>
       </header>
     </div>
   );
